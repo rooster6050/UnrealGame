b094deae94dda30b3c59e83cabc04b15f3f6eba1
GachiPC\Ed <edward.hawksworth@outlook.com>
2018-03-19 20:30:21 +0000

    Initial commit

---
+++ Source/UnrealGame/UnrealGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAME_API AUnrealGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
